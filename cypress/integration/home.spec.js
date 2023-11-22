24662b468217320ff1df1fa7be4c300727532d8e
JP Saliba <jpsaliba@outlook.com>
2023-11-22 15:25:54 -0500

    Initialize cypress and basic home page tests.

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,11 @@
+describe("Home", () => {
+  it("should navigate to the home page", () => {
+    cy.visit("/");
+  });
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+});
