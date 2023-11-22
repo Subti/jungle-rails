faaf700f5d349d644cdfb710b7b3c5547cd98149
JP Saliba <jpsaliba@outlook.com>
2023-11-22 17:03:51 -0500

    Fix login page styling to be similar to sign up as well as add class for identification. Implement rest of user authentication.

--- cypress/integration/user_login.spec.js
+++ cypress/integration/user_login.spec.js
@@ -17,7 +17,44 @@ describe("Home", () => {
   it("There is a signup form", () => {
     cy.get(".signup-form").should("be.visible");
   });
-  it("There is a login button", () => {
-    cy.get(".nav-link").contains("Login").should("be.visible");
+  it("Should fill out the signup form", () => {
+    cy.get("input[name='user[name]']").type("Test User");
+    cy.get("input[name='user[email]']").type("test@test.test");
+    cy.get("input[name='user[password]']").type("password");
+    cy.get("input[name='user[password_confirmation]']").type("password");
+  });
+  it("Click on the signup button", () => {
+    cy.get("input[type='submit']").click();
+  });
+  it("Is redirected to the home page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  it("There is a logout button", () => {
+    cy.get(".nav-link").contains("Logout").should("be.visible");
+  });
+  it("Click on the logout button", () => {
+    cy.get(".nav-link").contains("Logout").click();
+  });
+  it("Is redirected to the login page", () => {
+    cy.get(".login-form").should("be.visible");
+  });
+  it("There is a login form", () => {
+    cy.get(".login-form").should("be.visible");
+  });
+  it("Should fill out the login form", () => {
+    cy.get("input[name='email']").type("test@test.test");
+    cy.get("input[name='password']").type("password");
+  });
+  it("Click the login button", () => {
+    cy.get("input[type='submit']").click();
+  });
+  it("Is redirected to the home page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  it("Logs the user into the account they just created", () => {
+    cy.get(".nav-link").contains("Test User").should("be.visible");
+  });
+  it("There is a logout button", () => {
+    cy.get(".nav-link").contains("Logout").should("be.visible");
   });
 });
